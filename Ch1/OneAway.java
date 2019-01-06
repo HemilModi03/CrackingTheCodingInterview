6005a51ba7be00cd09dd41aac67737e25603e151
HemilModi03 <hemil.modi88@gmail.com>
2019-01-06 09:09:32 -0600

    Initial commit

---
+++ Ch1/OneAway.java
@@ -0,0 +1,75 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Ch1;
+
+/**
+ *
+ * @author hemil
+ */
+public class OneAway {
+    
+    public static boolean oneReplaceAway(String s1, String s2)
+    {
+        int check = 0;
+        for(int i=0; i<s1.length() ; i++){
+            if(s1.charAt(i) != s2.charAt(i)){
+                check++;
+                if(check > 1)
+                    return false;
+            }    
+        }
+        return true;
+    }
+    
+    public static boolean oneInsertDeleteAway(String s1, String s2)
+    {
+        int check = 0, i=0, j=0;
+        
+        while(i < s1.length() && j < s2.length() ){
+            
+            if(s1.charAt(i) != s2.charAt(j)){
+                i++;
+                check++;
+                if(check > 1)
+                    return false;
+            }
+            else{
+                i++;j++;
+            }
+        }
+        return true;
+    }
+    
+    public static boolean oneEditAway(String s1, String s2){
+    
+        if(s1.length() == s2.length()){
+            return oneReplaceAway(s1,s2);
+        }
+        else{
+            if(s1.length() > s2.length()){
+                if(s1.length() > s2.length()+1)
+                    return false;
+                
+                return oneInsertDeleteAway(s1,s2);
+            }
+            else if (s2.length() > s1.length()){
+                if(s2.length() > s1.length()+1)
+                    return false;
+                
+                return oneInsertDeleteAway(s2,s1);
+            }
+                
+        }
+        return true;
+    }
+    
+    public static void main(String[] args) {
+        String a = "pale";
+        String b = "bake";
+        boolean isOneEdit = oneEditAway(a, b);
+        System.out.println(a + ", " + b + ": " + isOneEdit);
+    }
+}
